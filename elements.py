07a3e09992019cf4c4f29500fc6a9d9ff78b97de
giacomells <matteo.giacomelli@studio.unibo.it>
2026-04-08 15:21:42 +0200

    Initial commit

---
+++ elements.py
@@ -0,0 +1,12 @@
+p = 400.0  # beam momentum (GeV/c)
+momentum = p  # beam momentum (GeV/c)
+Brho = p * 3.3356  # beam rigidity ???
+
+
+# Define constants for the simulation
+N_EX = 10e-6  # Normalized emittance in the x direction
+N_EY = 5e-6  # Normalized emittance in the y direction
+DPP = 1e-4  # Momentum spread
+
+# Set the septum aperture size
+septum_aperture_size = 68e-3  # Size of the septum aperture in meters
